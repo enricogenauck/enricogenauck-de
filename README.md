c1353753c3b6adb11cb457d927771f1225bf0791
Enrico Genauck <kontakt@enricogenauck.de>
2015-01-19 06:31:49 +0100

    new dawn, total reset

--- README.md
+++
@@ -1,6 +0,0 @@
-Headquarters
-============
-
-This code powers [enricogenauck.de](http://enricogenauck.de) and is basically
-a personal playground for different web technologies and
-features.
--- rakefile.rb
+++
@@ -1,8 +0,0 @@
-require 'rubygems'
-require 'rake'
-require File.dirname(__FILE__) + '/application.rb'
-
-desc "Cleans the redis cache"
-task "cron" do
-  puts "Flushing redis cache: " + REDIS.flushdb
-end
