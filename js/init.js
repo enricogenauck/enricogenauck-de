eafe41c3bc6c95a8ae7df6d9f38108a28eae3c81
Enrico Genauck <kontakt@enricogenauck.de>
2015-01-19 07:16:06 +0100

    adds impress

--- js/init.js
+++ js/init.js
@@ -7,7 +7,7 @@
 (function($) {
 
 	var settings = {
-		
+
 		// Parallax background effect?
 			parallax: true,
 
@@ -20,31 +20,31 @@
 		reset: 'full',
 		containers: '100%',
 		breakpoints: {
-			global: { href: 'css/style.css', grid: { gutters: ['2.5em', 0] } },
-			xlarge: { media: '(max-width: 1800px)', href: 'css/style-xlarge.css' },
-			large: { media: '(max-width: 1280px)', href: 'css/style-large.css', grid: { gutters: ['2em', 0] } },
-			medium: { media: '(max-width: 980px)', href: 'css/style-medium.css'},
-			small: { media: '(max-width: 736px)', href: 'css/style-small.css', grid: { gutters: ['1.5em', 0], zoom: 2 }, viewport: { scalable: false } },
-			xsmall: { media: '(max-width: 480px)', href: 'css/style-xsmall.css', grid: { zoom: 3 } }
+			global: { href: '/css/style.css', grid: { gutters: ['2.5em', 0] } },
+			xlarge: { media: '(max-width: 1800px)', href: '/css/style-xlarge.css' },
+			large: { media: '(max-width: 1280px)', href: '/css/style-large.css', grid: { gutters: ['2em', 0] } },
+			medium: { media: '(max-width: 980px)', href: '/css/style-medium.css'},
+			small: { media: '(max-width: 736px)', href: '/css/style-small.css', grid: { gutters: ['1.5em', 0], zoom: 2 }, viewport: { scalable: false } },
+			xsmall: { media: '(max-width: 480px)', href: '/css/style-xsmall.css', grid: { zoom: 3 } }
 		}
 	});
 
 	$(function() {
-		
+
 		var $window = $(window),
 			$body = $('body'),
 			$header = $('#header');
 
 		// Disable animations/transitions until the page has loaded.
 			$body.addClass('is-loading');
-			
+
 			$window.on('load', function() {
 				$body.removeClass('is-loading');
 			});
 
 		// Touch?
 			if (skel.vars.isMobile) {
-			
+
 				// Turn on touch mode.
 					$body.addClass('is-touch');
 
@@ -52,13 +52,13 @@
 					window.setTimeout(function() {
 						$window.scrollTop($window.scrollTop() + 1);
 					}, 0);
-			
+
 			}
 
 		// Forms (IE<10).
 
 			if (skel.vars.IEVersion < 10) {
-				
+
 				var $form = $('form');
 
 				if ($form.length > 0) {
@@ -67,9 +67,9 @@
 					$form.n33_formerize();
 
 				}
-			
+
 			}
-					
+
 		// Header.
 
 			// Parallax background.
@@ -82,29 +82,29 @@
 				if (settings.parallax) {
 
 					skel.change(function() {
-						
+
 						if (skel.isActive('medium')) {
-							
+
 							$window.off('scroll.strata_parallax');
 							$header.css('background-position', 'top left, center center');
-						
+
 						}
 						else {
-							
+
 							$header.css('background-position', 'left 0px');
-					
+
 							$window.on('scroll.strata_parallax', function() {
 								$header.css('background-position', 'left ' + (-1 * (parseInt($window.scrollTop()) / settings.parallaxFactor)) + 'px');
 							});
-							
+
 						}
-					
+
 					});
-					
+
 				}
 
 		// Main Sections: Two.
-		
+
 			// Lightbox gallery.
 				$('#two').poptrox({
 					caption: function($a) { return $a.next('h3').text(); },
